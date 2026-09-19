a68521b7a26ca0712a8b813e7c5e0924af0dc1e5
shahankhan47 <75560028+shahankhan47@users.noreply.github.com>
2026-09-19 17:29:54 +0530

    Uncomment Jest configuration for testing

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-// module.exports = {
-//   testEnvironment: 'node',
-//   roots: ['<rootDir>/test'],
-//   testMatch: ['**/*.test.ts'],
-//   transform: {
-//     '^.+\\.tsx?$': 'ts-jest'
-//   }
-// };
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  }
+};
